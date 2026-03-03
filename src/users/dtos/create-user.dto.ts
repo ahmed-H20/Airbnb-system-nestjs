5d56cc3e6fb3ad6dda7f6aaa8173832792fe98f3
ahmed-H20 <ahmedheshamahah8@gmail.com>
2026-03-03 12:58:52 +0200

    Edit user DTO

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsPhoneNumber, IsString, Length } from 'class-validator';
+import {
+  IsEmail,
+  IsPhoneNumber,
+  IsString,
+  Length,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   @Length(3, 20)
@@ -8,6 +14,7 @@ export class CreateUserDto {
   @IsEmail()
   readonly email: string;
 
+  @MinLength(6, { message: 'Password must be at least 6 characters' })
   @IsString()
   readonly password: string;
 
