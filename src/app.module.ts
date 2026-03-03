eb5a4503b709bde36744f5d4e4e7ee7345567bcd
ahmed-H20 <ahmedheshamahah8@gmail.com>
2026-03-03 12:59:06 +0200

    Create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { MongoDbModule } from './mongo-db/mongo-db.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, MongoDbModule],
+  imports: [ConfigModule.forRoot(), UsersModule, MongoDbModule, AuthModule],
   providers: [],
 })
 export class AppModule {}
