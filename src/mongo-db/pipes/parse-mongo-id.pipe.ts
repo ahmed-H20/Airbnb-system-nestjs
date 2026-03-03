62ecd7fd670538ac9bd5d7ef0d36757284a7da62
ahmed-H20 <ahmedheshamahah8@gmail.com>
2026-03-03 11:53:42 +0200

    Create Mongo DB

---
+++ src/mongo-db/pipes/parse-mongo-id.pipe.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+
+export class ParseMongoIdPipe implements PipeTransform<string> {
+  transform(value: string, metadata: ArgumentMetadata): string {
+    // validate that value is a valid mongo id
+    const isValidMongoId = /^[a-f\d]{24}$/i.test(value);
+    if (!isValidMongoId) {
+      throw new BadRequestException(`${value} is not a valid mongo id`);
+    }
+
+    return value;
+  }
+}
