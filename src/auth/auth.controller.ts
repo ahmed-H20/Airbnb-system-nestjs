b80dfd9e0c6b505e7d68114a5564f6473dcfa906
ahmed-H20 <ahmedheshamahah8@gmail.com>
2026-03-04 11:04:54 +0200

    Finish signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/create-user.dto';
+import { AuthService } from './auth.service';
+import type { Response } from 'express';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly auth: AuthService) {}
+
+  // =============Signup==============
+  @Post()
+  async signup(
+    @Res({ passthrough: true }) res: Response,
+    @Body() signupUser: CreateUserDto,
+  ) {
+    const token = await this.auth.signup(signupUser);
+
+    res.cookie('token', token, {
+      httpOnly: true,
+    });
+
+    return { message: 'Signup in successfully' };
+  }
+
+  // =============Login=============
+}
