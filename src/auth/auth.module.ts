1cd3cfb0ed281e17ecf69532f1fd9380d038382d
ahmed-H20 <ahmedheshamahah8@gmail.com>
2026-03-04 09:54:00 +0200

    Create auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({})
+@Module({
+  providers: [AuthService],
+})
 export class AuthModule {}
