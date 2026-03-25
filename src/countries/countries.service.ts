f018095c7ff7c4ef7634abd5dbccaf84568b5706
ahmed-H20 <ahmedheshamahah8@gmail.com>
2026-03-25 09:30:15 +0200

    Create country service

---
+++ src/countries/countries.service.ts
@@ -0,0 +1,63 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Country } from './schemas/countries.schema';
+import { Model } from 'mongoose';
+import { CreateCountryDto } from './dtos/create-country.dto';
+import { UpdateCountryDto } from './dtos/update-country.dto';
+
+@Injectable()
+export class CountriesService {
+  constructor(
+    @InjectModel(Country.name) private countryModel: Model<Country>,
+  ) {}
+
+  // Create new user
+  createCountry = async (country: CreateCountryDto): Promise<Country> => {
+    const newCountry = await this.countryModel.create(country);
+    return newCountry;
+  };
+
+  // Get all Countries
+  getAllCountries = async (): Promise<Country[]> => {
+    const Countries = await this.countryModel.find();
+    return Countries;
+  };
+
+  // Get one user
+  getUserById = async (id: string): Promise<Country> => {
+    const user = await this.countryModel.findById(id);
+    if (!user) {
+      throw new NotFoundException(`there is no country for this id: ${id}`);
+    }
+    return user;
+  };
+
+  // Update user
+  updateCountry = async (
+    id: string,
+    UpdateUserdata: UpdateCountryDto,
+  ): Promise<Country> => {
+    const updatedCountry = await this.countryModel.findByIdAndUpdate(
+      id,
+      UpdateUserdata,
+      { new: true },
+    );
+
+    if (!updatedCountry) {
+      throw new NotFoundException(`there is no country for this id: ${id}`);
+    }
+
+    return updatedCountry;
+  };
+
+  // Delete user
+  deleteCountry = async (id: string): Promise<object> => {
+    const country = await this.countryModel.findByIdAndDelete(id);
+    if (!country) {
+      throw new NotFoundException(`There is no country for this id: ${id}`);
+    }
+    return {
+      message: `Country with id: ${id} deleted successfully✅`,
+    };
+  };
+}
